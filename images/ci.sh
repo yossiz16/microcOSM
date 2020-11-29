8f4d0594bf921fe25df9940cb29086bddf1932d4
yossiz16 <yossiz16@gmail.com>
2020-11-29 14:22:05 +0200

    Update ci.sh

--- images/ci.sh
+++ images/ci.sh
@@ -4,13 +4,14 @@ set -e
 SHA=`git rev-parse HEAD`
 TAG=`git describe --abbrev=0 --tags`
 ECR=645470188746.dkr.ecr.eu-west-1.amazonaws.com
+PROJECT=microcosm
 echo $TAG
 for d in */ ; do
   if [ -f "${d}Dockerfile" ]; then
     filename=$(basename -- "$d")
     foldername=${filename%.*}
     echo $filename
-    echo -e "running docker build -q -t ${SHA} -t ${ECR}/${foldername}:${TAG}"
+    echo -e "running docker build -q -t ${SHA} -t ${ECR}/${PROJECT}${foldername}:${TAG}"
     cd $foldername
     docker build -q -t $SHA -t $ECR/$foldername:$TAG .
     docker push $ECR/$foldername:$TAG

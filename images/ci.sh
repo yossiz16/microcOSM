d44cc490125fe33eb4da1602919882ed91cac835
yossiz16 <yossiz16@gmail.com>
2020-11-29 13:02:01 +0200

    ci(github-actions): add images push to ECR workflow (#56)
    * Create main.yml
    
    * Create ci.sh
    
    * Update main.yml
    
    * Update ci.sh
    
    * Update main.yml

---
+++ images/ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo $PWD
+set -e
+SHA=`git rev-parse HEAD`
+TAG=`git tag`
+ECR=http://645470188746.dkr.ecr.eu-west-1.amazonaws.com
+echo $TAG
+for d in */ ; do
+  if [ -f "${d}Dockerfile" ]; then
+    filename=$(basename -- "$d")
+    foldername=${filename%.*}
+    echo $filename
+    echo -e "running docker build -t microcosm-${foldername}:$TAG ${d}${NC}"
+    docker build -q -t $SHA -t $ECR/$foldername:$TAG
+    docker push $ECR/$foldername:$TAG
+  fi
+done
